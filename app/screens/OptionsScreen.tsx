23239d79fc793a8323050cf65b4163823d8f85c6
mr-ristic <marko.risticcc@gmail.com>
2023-11-06 19:09:05 +0100

    generate screens

---
+++ app/screens/OptionsScreen.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react"
+import { observer } from "mobx-react-lite"
+import { ViewStyle } from "react-native"
+import { AppStackScreenProps } from "app/navigators"
+import { Screen, Text } from "app/components"
+// import { useNavigation } from "@react-navigation/native"
+// import { useStores } from "app/models"
+
+interface OptionsScreenProps extends AppStackScreenProps<"Options"> {}
+
+export const OptionsScreen: FC<OptionsScreenProps> = observer(function OptionsScreen() {
+  // Pull in one of our MST stores
+  // const { someStore, anotherStore } = useStores()
+
+  // Pull in navigation via hook
+  // const navigation = useNavigation()
+  return (
+    <Screen style={$root} preset="scroll">
+      <Text text="options" />
+    </Screen>
+  )
+})
+
+const $root: ViewStyle = {
+  flex: 1,
+}
--- app/screens/index.ts
+++ app/screens/index.ts
@@ -1,3 +1,7 @@
 export * from "./WelcomeScreen"
 export * from "./ErrorScreen/ErrorBoundary"
 // export other screens here
+export * from "./HomeScreen"
+export * from "./CalendarScreen"
+export * from "./ChatScreen"
+export * from "./OptionsScreen"
