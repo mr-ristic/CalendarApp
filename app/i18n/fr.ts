c13a24ca9c7f0a0207c504aef80b6108c4eaf411
mr-ristic <marko.risticcc@gmail.com>
2023-11-06 19:20:11 +0100

    add screen tabs transltions

--- app/i18n/fr.ts
+++ app/i18n/fr.ts
@@ -1,31 +1,47 @@
-import { Translations } from "./en"
+import { Translations } from './en';
 
 const fr: Translations = {
   common: {
-    ok: "OK !",
-    cancel: "Annuler",
-    back: "Retour",
+    ok: 'OK !',
+    cancel: 'Annuler',
+    back: 'Retour'
   },
   welcomeScreen: {
     postscript:
       "psst  — Ce n'est probablement pas à quoi ressemble votre application. (À moins que votre designer ne vous ait donné ces écrans, dans ce cas, mettez la en prod !)",
-    readyForLaunch: "Votre application, presque prête pour le lancement !",
-    exciting: "(ohh, c'est excitant !)",
+    readyForLaunch: 'Votre application, presque prête pour le lancement !',
+    exciting: "(ohh, c'est excitant !)"
   },
   errorScreen: {
     title: "Quelque chose s'est mal passé !",
     friendlySubtitle:
       "C'est l'écran que vos utilisateurs verront en production lorsqu'une erreur sera lancée. Vous voudrez personnaliser ce message (situé dans `app/i18n/fr.ts`) et probablement aussi la mise en page (`app/screens/ErrorScreen`). Si vous voulez le supprimer complètement, vérifiez `app/app.tsx` pour le composant <ErrorBoundary>.",
-    reset: "RÉINITIALISER L'APPLICATION",
+    reset: "RÉINITIALISER L'APPLICATION"
   },
   emptyStateComponent: {
     generic: {
-      heading: "Si vide... si triste",
+      heading: 'Si vide... si triste',
       content:
         "Aucune donnée trouvée pour le moment. Essayez de cliquer sur le bouton pour rafraîchir ou recharger l'application.",
-      button: "Essayons à nouveau",
-    },
+      button: 'Essayons à nouveau'
+    }
   },
-}
+  homeScreen: {
+    title: 'Home',
+    body: 'Cooming soon!'
+  },
+  calendarScreen: {
+    title: 'Calendar',
+    body: 'Cooming soon!'
+  },
+  chatScreen: {
+    title: 'Chat',
+    body: 'Cooming soon!'
+  },
+  optionsScreen: {
+    title: 'Options',
+    body: 'Cooming soon!'
+  }
+};
 
-export default fr
+export default fr;
